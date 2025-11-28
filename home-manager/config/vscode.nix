62ca6b89c879bce726cb230583037d2350997e2e
PedroDiniz <pedro_nork@hotmail.com>
2025-11-28 16:42:27 -0300

    Refactor VSCode and WezTerm wrapper scripts: streamline environment variables for improved compatibility and clarity

--- home-manager/config/vscode.nix
+++ home-manager/config/vscode.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 {
   programs.vscode.enable = true;
-  # Wrapper script for VSCode ensuring host/Nix GL libs are visible
+  # Minimal wrapper: rely on Nix RPATH; don't inject host /usr/lib
   home.file.".local/bin/code".text = ''#!/usr/bin/env sh
-export LIBGL_DRIVERS_PATH="/usr/lib/dri:${pkgs.mesa.drivers}/lib/dri:''${LIBGL_DRIVERS_PATH:-}"
-export __EGL_VENDOR_LIBRARY_DIRS="/usr/share/glvnd/egl_vendor.d:${pkgs.libglvnd}/share/glvnd/egl_vendor.d:''${__EGL_VENDOR_LIBRARY_DIRS:-}"
-export LD_LIBRARY_PATH="$HOME/.nix-profile/lib:/usr/lib:''${LD_LIBRARY_PATH:-}"
 exec "${pkgs.vscode}/bin/code" --disable-gpu --use-gl=swiftshader --ozone-platform-hint=auto "$@"
 '';
   home.file.".local/bin/code".executable = true;
--- home-manager/config/wezterm.nix
+++ home-manager/config/wezterm.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }:
 let
   wezWrapped = pkgs.writeShellScriptBin "wezterm" ''
-    # Minimal SteamOS/KDE-friendly env: use host GL paths, force X11 + Software
-    export LIBGL_DRIVERS_PATH="/usr/lib/dri:${pkgs.mesa.drivers}/lib/dri:''${LIBGL_DRIVERS_PATH:-}"
-    export __EGL_VENDOR_LIBRARY_DIRS="/usr/share/glvnd/egl_vendor.d:${pkgs.libglvnd}/share/glvnd/egl_vendor.d:''${__EGL_VENDOR_LIBRARY_DIRS:-}"
-    export LD_LIBRARY_PATH="$HOME/.nix-profile/lib:/usr/lib:''${LD_LIBRARY_PATH:-}"
-    export WINIT_UNIX_BACKEND="x11"
-    export WEZTERM_FRONTEND="Software"
+    # Clean wrapper: avoid overriding dynamic loader with host libs
+    # Allow user to set WINIT/FRONTEND externally; provide safe defaults
+    : "${WINIT_UNIX_BACKEND:=x11}"
+    : "${WEZTERM_FRONTEND:=OpenGL}"
+    export WINIT_UNIX_BACKEND WEZTERM_FRONTEND
     exec "${pkgs.wezterm}/bin/wezterm" "$@"
   '';
   xterm = pkgs.writeShellScriptBin "xterm" ''${wezWrapped}/bin/wezterm "$@"'';
