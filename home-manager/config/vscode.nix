f1301aba90116f59d0cb53a4c8edb6ca37d92c90
PedroDiniz <pedro_nork@hotmail.com>
2025-11-28 16:31:08 -0300

    Refine VSCode wrapper script: add comment for clarity on GL library visibility

--- home-manager/config/vscode.nix
+++ home-manager/config/vscode.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
   programs.vscode.enable = true;
-  home.file".local/bin/code".text = ''#!/usr/bin/env sh
+  # Wrapper script for VSCode ensuring host/Nix GL libs are visible
+  home.file.".local/bin/code".text = ''#!/usr/bin/env sh
 export LIBGL_DRIVERS_PATH="/usr/lib/dri:${pkgs.mesa.drivers}/lib/dri:''${LIBGL_DRIVERS_PATH:-}"
 export __EGL_VENDOR_LIBRARY_DIRS="/usr/share/glvnd/egl_vendor.d:${pkgs.libglvnd}/share/glvnd/egl_vendor.d:''${__EGL_VENDOR_LIBRARY_DIRS:-}"
 export LD_LIBRARY_PATH="$HOME/.nix-profile/lib:/usr/lib:''${LD_LIBRARY_PATH:-}"
 exec "${pkgs.vscode}/bin/code" --disable-gpu --use-gl=swiftshader --ozone-platform-hint=auto "$@"
 '';
-  home.file".local/bin/code".executable = true;
+  home.file.".local/bin/code".executable = true;
 }
